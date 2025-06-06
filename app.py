dab5f64241c2cee004e880ab9e0a36ed65b20266
MiKerfr <makbudnik@yandex.ru>
2025-06-06 19:08:49 +0300

    Add /metrics route to count /time requests

--- app.py
+++ app.py
@@ -9,3 +9,17 @@ def get_time():
 
 if __name__ == "__main__":
     app.run()
+time_requests_count = 0
+
+@app.route('/time', methods=['GET'])
+def get_time():
+    global time_requests_count
+    time_requests_count += 1
+    return jsonify({'time': int(time.time())})
+
+@app.route('/metrics', methods=['GET'])
+def get_metrics():
+    return jsonify({'count': time_requests_count})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
